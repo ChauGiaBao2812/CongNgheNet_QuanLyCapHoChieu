b0d29ead53cb8d89ecce06914e033c722f92e634
ChauGiaBao2812 <giabao28122004@gmail.com>
2025-05-19 15:12:44 +0700

    Thêm ViewModel

---
+++ QuanLiHoChieu/Models/ViewModels/PassportFormVM.cs
@@ -0,0 +1,80 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QuanLiHoChieu.Models.ViewModels
+{
+    public class PassportFormVM
+    {
+        [Required]
+        [StringLength(20)]
+        public string FormID { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(@"^0\d{11}$", ErrorMessage = "CCCD không hợp lệ.")]
+        public string CCCD { get; set; } = null!;
+
+        [Required]
+        public string HoTen { get; set; } = null!;
+
+        [Required]
+        [StringLength(10)]
+        public string GioiTinh { get; set; } = null!;
+
+        [Required]
+        public DateTime NgaySinh { get; set; }
+
+        [Required]
+        public string NoiSinh { get; set; } = null!;
+
+        [Required]
+        public DateTime NgayCap { get; set; }
+
+        [Required]
+        public string NoiCap { get; set; } = null!;
+
+        [Required]
+        [StringLength(50)]
+        public string DanToc { get; set; } = null!;
+
+        [StringLength(50)]
+        public string? TonGiao { get; set; }
+
+        [Required]
+        public string SDT { get; set; } = null!;
+
+        // Địa chỉ thường trú
+        [Required]
+        public string ttTinhThanh { get; set; } = null!;
+
+        [Required]
+        public string ttQuanHuyen { get; set; } = null!;
+
+        [Required]
+        public string ttPhuongXa { get; set; } = null!;
+
+        [Required]
+        public string ttSoNhaDuong { get; set; } = null!;
+
+        // Địa chỉ tạm trú
+        [Required]
+        public string thtTinhThanh { get; set; } = null!;
+
+        [Required]
+        public string thtQuanHuyen { get; set; } = null!;
+
+        [Required]
+        public string thtPhuongXa { get; set; } = null!;
+
+        [Required]
+        public string thtSoNhaDuong { get; set; } = null!;
+
+        // Optional fields
+        public string? NgheNghiep { get; set; }
+        public string? CoQuan { get; set; }
+        public string? DiaChiCoQuan { get; set; }
+        public string? HoTenCha { get; set; }
+        public DateTime? NgaySinhCha { get; set; }
+        public string? HoTenMe { get; set; }
+        public DateTime? NgaySinhMe { get; set; }
+    }
+
+}
