20150271c5fc311547bf31387297c135fe26dba0
Le Tuan Dat <dat261303@gmail.com>
2025-05-20 10:38:26 +0700

    Do du lieu vao view LuuTru

--- QuanLiHoChieu/Controllers/XacThucController.cs
+++ QuanLiHoChieu/Controllers/XacThucController.cs
@@ -1,11 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using QuanLiHoChieu.Data;
 
 namespace QuanLiHoChieu.Controllers
 {
     public class XacThucController : Controller
     {
+        private readonly PassportDbContext _context;
+        public XacThucController(PassportDbContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
+            
             return View();
         }
     }
